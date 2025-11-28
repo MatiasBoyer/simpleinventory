ffa0c4a8aeb9fc9cf9ba405bfbd8130208a1293a
Matías Boyer <matiasboyer7@gmail.com>
2025-11-28 18:27:17 -0300

    feat: mobile device initialization

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,23 @@
 import './App.css';
 import { Route, Routes } from 'react-router';
 import Main from '@pages/main/main';
+import CleanClassnames from './utils/functions/CleanClassnames';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-    </Routes>
+    <div className="w-full flex items-center justify-center">
+      <div
+        className={CleanClassnames(`
+        h-screen
+        w-full md:w-[30%] md:max-w-[30%]
+        bg-stone-100
+        `)}
+      >
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
