3c48816447e44e215a00446b301bf5a902f74124
Matías Boyer <matiasboyer7@gmail.com>
2025-12-04 09:28:32 -0300

    feat: json middleware

--- backend/src/config/setup.js
+++ backend/src/config/setup.js
@@ -1,12 +1,14 @@
 import errorHandler from '#middlewares/errorhandler.js';
 import routesIndex from '#routes/index.js';
 import { GetAIClient } from '#config/ai.js';
+import { json } from 'express';
 
 /**
  * @param {import('express').Application} app - express app
  */
 function setup(app) {
   GetAIClient();
+  app.use(json({ limit: '10mb' }));
   app.use(routesIndex);
   app.use(errorHandler);
 }
