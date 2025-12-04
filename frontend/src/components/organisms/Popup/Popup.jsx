7b48946cc9b18421eea7d31faba73672632bf810
Matías Boyer <matiasboyer7@gmail.com>
2025-12-04 16:30:19 -0300

    style: pop-up refactor to the new style

--- frontend/src/components/organisms/Popup/Popup.jsx
+++ frontend/src/components/organisms/Popup/Popup.jsx
@@ -6,19 +6,19 @@ export default function Popup({ children, title, onAccept, onCancel }) {
   return (
     <div
       className={CleanClassnames(`
-        w-[90%] min-h-[25%] bg-stone-300 rounded-md overflow-hidden
+        w-[90%] min-h-[25%] rounded-md overflow-hidden border
         grid ${title ? 'grid-rows-[20%_1fr_20%]' : 'grid-rows-[1fr_20%]'}
     `)}
     >
       {title && (
-        <div className="flex items-center justify-center bg-stone-400">
-          <Text>{title}</Text>
+        <div className="flex items-center justify-center border-b bg-neutral-200">
+          <Text className="text-xl font-bold">{title}</Text>
         </div>
       )}
-      <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center bg-neutral-100">
         {children}
       </div>
-      <div className="flex items-center justify-center gap-2 bg-stone-400">
+      <div className="flex items-center justify-center gap-2 border-t bg-neutral-200">
         <Button
           onClick={() => {
             onAccept?.();
