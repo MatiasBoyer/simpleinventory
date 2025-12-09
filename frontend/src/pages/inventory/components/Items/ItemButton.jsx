c59606679fe6c7bfaea6ee9f27e031d9927446f7
Matías Boyer <matiasboyer7@gmail.com>
2025-12-09 10:46:20 -0300

    refactor: itembutton, itementry and input code ordering and setting quantity

--- frontend/src/pages/inventory/components/Items/ItemButton.jsx
+++ frontend/src/pages/inventory/components/Items/ItemButton.jsx
@@ -1,17 +1,23 @@
 import Button from '@/components/atoms/Button';
 import CleanClassnames from '@/utils/functions/CleanClassnames';
 
-function ItemButton({ type, onClick }) {
+function ItemButton({ children, type, onClick }) {
+  const typeClass = {
+    '+': 'border-r-1 rounded-r-md',
+    '-': 'border-l-1 rounded-l-md',
+    ' ': 'border-1',
+  };
+
   return (
     <Button
       replaceClassname={CleanClassnames(`
         bg-stone-200
-        border-y ${type === '+' ? 'border-r-1 rounded-r-md' : 'border-l-1 rounded-l-md'}
+        border-y ${typeClass[type ?? ' '] ?? typeClass[' ']}
         cursor-pointer
         `)}
       onClick={onClick}
     >
-      {type}
+      {children ?? type}
     </Button>
   );
 }
--- frontend/src/pages/inventory/components/Items/ItemEntry.jsx
+++ frontend/src/pages/inventory/components/Items/ItemEntry.jsx
@@ -13,17 +13,59 @@ function ItemEntry({
   onItemRename,
   difference = null,
 }) {
+  const int_onRemoveItem = () => {
+    AddPopup({
+      children: <>Are you sure?</>,
+      title: 'Confirmation',
+      onAccept: () => onRemoveItem(item.id),
+    });
+  };
+  const int_onModifyItemName = () => {
+    AddPopup({
+      title: 'Modify item name',
+      children: (
+        <>
+          <Text className="w-full">New name</Text>
+          <Input id="newName" maxLength={20} />
+        </>
+      ),
+      onAccept: (values) => {
+        if (values?.newName) onItemRename?.(values.newName);
+      },
+    });
+  };
+  const int_onButtonMinus = () => {
+    onSumItem(item.id, item.quantity + -1);
+  };
+  const int_onButtonInput = () => {
+    AddPopup({
+      title: 'Modify item quantity',
+      children: (
+        <>
+          <Text className="w-full">New quantity</Text>
+          <Input
+            id="newQuantity"
+            className="w-full"
+            maxLength={999}
+            type="number"
+            defaultValue={item?.quantity ?? 0}
+          />
+        </>
+      ),
+      onAccept: (values) => {
+        if (values?.newQuantity) onSetItemQty(item.id, values.newQuantity);
+      },
+    });
+  };
+  const int_onButtonPlus = () => {
+    onSumItem(item.id, item.quantity + 1);
+  };
+
   return (
     <div className="w-[95%] p-2 grid grid-cols-[5%_1fr_20%] border-b flex flex-row justify-between items-center">
       <div className="flex items-center justify-center">
         <Button
-          onClick={() => {
-            AddPopup({
-              children: <>Are you sure?</>,
-              title: 'Confirmation',
-              onAccept: () => onRemoveItem(item.id),
-            });
-          }}
+          onClick={int_onRemoveItem}
           replaceClassname="border-0 flex items-center w-full h-full cursor-pointer"
         >
           <FaRegTrashAlt />
@@ -32,20 +74,7 @@ function ItemEntry({
       <div className="w-full flex flex-row justify-between items-center gap-2">
         <div
           className="w-full max-w-[60%] px-2"
-          onDoubleClick={() =>
-            AddPopup({
-              title: 'Modify item name',
-              children: (
-                <>
-                  <Text className="w-full">New name</Text>
-                  <Input id="newName" maxLength={20} />
-                </>
-              ),
-              onAccept: (values) => {
-                if (values?.newName) onItemRename?.(values.newName);
-              },
-            })
-          }
+          onDoubleClick={int_onModifyItemName}
         >
           <Text className="w-full">{item.item_text}</Text>
         </div>
@@ -66,30 +95,9 @@ function ItemEntry({
         )}
       </div>
       <div className="grid grid-cols-[30%_auto_30%]">
-        <ItemButton
-          type="-"
-          onClick={() => onSumItem(item.id, item.quantity + -1)}
-        />
-        <input
-          value={item.quantity}
-          onChange={(e) => {
-            const n = parseInt(e.target.value);
-
-            if (Number.isNaN(n)) return;
-
-            onSetItemQty(item.id, n);
-          }}
-          className={`
-            w-full text-center bg-stone-200 border
-            [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none
-          `}
-          type="number"
-          disabled={true} // MODIFY ME
-        />
-        <ItemButton
-          type="+"
-          onClick={() => onSumItem(item.id, item.quantity + 1)}
-        />
+        <ItemButton type="-" onClick={int_onButtonMinus} />
+        <ItemButton onClick={int_onButtonInput}>{item.quantity}</ItemButton>
+        <ItemButton type="+" onClick={int_onButtonPlus} />
       </div>
     </div>
   );
